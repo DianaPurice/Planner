3c5d71bda409b9e39117d4c30081d9e0c4ea1f76
kostyscp <5purid83@solent.ac.uk>
2023-04-04 23:13:22 +0100

    computer not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // existing elements
 var currentDayEl = $("#currentDay");
-var jumbotron = $(".jumbotron");
-
-// style elements
-jumbotron.css("text-align", "center");
-jumbotron.css("border-bottom", "10px solid black");
-
-
+var timeslot1 = $("#9am");
+var timeslot2 = $("#10am");
+var timeslot2 = $("#11am");
+var timeslot3 = $("#12am");
+var timeslot4 = $("#1pm");
+var timeslot5 = $("#2pm");
+var timeslot6 = $("#3pm");
+var timeslot7 = $("#4pm");
+var timeslot8 = $("#5pm");
 
 // display current date
 function currentDay() {
@@ -15,3 +17,23 @@ function currentDay() {
 }
 
 currentDay();
+
+function currentHour() {
+  var hourNow = moment().format("h:mm:ss a");
+  return hourNow;
+}
+
+function checkHour() {
+  var hourNow = currentHour();
+  var hourLenght = hourNow.lenght;
+  console.log(typeof hourNow.length);
+  console.log(hourNow);
+  console.log(hourNow);
+  if (hourLenght === 11) {
+    console.log("here");
+  } else {
+    console.log("hereee");
+  }
+  console.log(hourNow);
+}
+checkHour();
